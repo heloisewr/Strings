0a162f2026d5eb7730b7a5c43f4ff621c4c5dd50
heloisewr <heloise.antunes.santos@escola.pr.gov.br>
2025-09-02 12:01:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {PALAVRAS_RUINS} from "./palavrasRuins.js"
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 botaoMostraPalavras.addEventListener('click',mostraPalavrasChave);
 function mostraPalavrasChave(){
@@ -8,6 +9,10 @@ function mostraPalavrasChave(){
 }
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    for (let i in palavras) {
+      palavras[i] = palavras[i].toLowerCase();
+    }
+    palavras = tiraPalavrasRuins(palavras);
      const frequencias = contaFrequencias(palavras);
      let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
      function ordenaPalavra(p1,p2) {
@@ -30,4 +35,17 @@ function contaFrequencias(palavras){
 }
 //console.log(frequencias);
     return frequencias;
+}
+function tiraPalavrasRuins(palavras) {
+  const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+
+  const palavrasBoas = [];
+
+  for (let palavra of palavras) {
+    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+      palavrasBoas.push(palavra);
+    }
+  }
+
+  return palavrasBoas;
 }
